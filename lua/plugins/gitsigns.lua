37ddf0c64e08f546f2759aec5a8d2bc213014a85
Islam Sulaiman <i.sulaiman@qoyod.com>
2025-01-14 09:14:31 +0200

    refactor: change the configuration for gitsigns package

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,20 +1,10 @@
 -- Adds git related signs to the gutter, as well as utilities for managing changes
 return {
   'lewis6991/gitsigns.nvim',
-  opts = {
-    signs = {
-      add = { text = '+' },
-      change = { text = '~' },
-      delete = { text = '_' },
-      topdelete = { text = '‾' },
-      changedelete = { text = '~' },
-    },
-    signs_staged = {
-      add = { text = '+' },
-      change = { text = '~' },
-      delete = { text = '_' },
-      topdelete = { text = '‾' },
-      changedelete = { text = '~' },
-    },
-  },
+  config = function()
+    require('gitsigns').setup()
+
+    vim.keymap.set('n', '<leader>gp', ':Gitsigns preview_hunk<CR>', {})
+    vim.keymap.set('n', '<leader>gb', ':Gitsigns toggle_current_line_blame<CR>', {})
+  end,
 }
