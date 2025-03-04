2b910c50a6d9d8eb863c8d27755814d73cdf982b
Islam Sulaiman <ma.eslam@yahoo.com>
2025-03-04 17:38:10 +0200

    feat: add find word to telescope
    It works by hovering on the word to search for
    then press leader+f+w

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -99,6 +99,7 @@ return {
     vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
     vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
     vim.keymap.set("n", "<leader>fo", builtin.oldfiles, { desc = "Find Old Files" })
+    vim.keymap.set("n", "<leader>fw", builtin.grep_string, { desc = "Find Word under Cursor" })
 
     -- Slightly advanced example of overriding default behavior and theme
     vim.keymap.set('n', '<leader>/', function()
