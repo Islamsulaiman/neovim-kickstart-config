5699c744bdcb943952f887340215e002e3224a94
Islam Sulaiman <ma.eslam@yahoo.com>
2025-03-04 18:55:19 +0200

    feat: add codesnap for taking code screenshots

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -47,4 +47,18 @@ return {
   {
     "tpope/vim-endwise", ft = { "ruby", "eruby" }
   },
+  {
+    "mistricky/codesnap.nvim",
+    build = "make build_generator",
+    version = "0.0.11",
+    keys = {
+      { "<leader>cc", "<cmd>CodeSnap<cr>", mode = "x", desc = "Save selected code snapshot into clipboard" },
+      { "<leader>cs", "<cmd>CodeSnapSave<cr>", mode = "x", desc = "Save selected code snapshot in ~/Pictures" },
+    },
+    opts = {
+      save_path = "~/Pictures",
+      has_breadcrumbs = true,
+      bg_theme = "bamboo",
+    },
+  },
 }
